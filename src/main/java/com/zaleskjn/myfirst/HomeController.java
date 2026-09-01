0c486397ae04936a72d07b328f4d4af66626b56b
JaxonZaleski <zaleskjn@mail.uc.edu>
2026-09-01 14:57:13 -0400

    added HomeController

--- src/main/java/com/zaleskjn/myfirst/HomeController.java
+++ src/main/java/com/zaleskjn/myfirst/HomeController.java
@@ -1,4 +1,13 @@
 package com.zaleskjn.myfirst;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index.html";
+    }
 }
